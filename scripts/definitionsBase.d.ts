5b801f842a1e5c913b5db9af479918294f5581df
SimeonC <simeon@thehavens.co.nz>
2021-01-08 13:42:08 +0900

    feat: add support for typescript definitions
    Base the definitions off of the font awesome way of doing things.
    This is required for TSC upgrade of @tablekit/icon.

---
+++ scripts/definitionsBase.d.ts
@@ -0,0 +1,22 @@
+export type IconPrefix = 'tci';
+export type IconPathData = string | string[];
+
+export interface IconLookup {
+  prefix: IconPrefix;
+  // IconName is defined in the code that will be generated at build time and bundled with this file.
+  iconName: IconName;
+}
+
+export interface IconDefinition extends IconLookup {
+  icon: [
+    number, // width
+    number, // height
+    string[], // ligatures
+    string, // unicode
+    IconPathData // svgPathData
+  ];
+}
+
+export interface IconPack {
+  [key: string]: IconDefinition;
+}
