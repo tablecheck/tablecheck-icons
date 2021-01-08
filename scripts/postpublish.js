75a7a39c8920db5341ebb8645242ceb556d7facf
SimeonC <simeon@thehavens.co.nz>
2021-01-08 16:42:48 +0900

    fix: make pre and post publish scripts include ts
    Also exclude config files as we don’t need those published

--- scripts/postpublish.js
+++ scripts/postpublish.js
@@ -5,8 +5,15 @@ const glob = require('glob');
 
 console.log(colors.blue('Cleaning up export files'));
 
-glob(path.join(process.cwd(), '*.js'), {}, (err, files) => {
-  files
-    .filter((filePath) => !/\.config\.js$/gi.test(path.basename(filePath)))
-    .forEach((filePath) => fs.removeSync(filePath));
+const files = glob.sync(path.join(process.cwd(), '*.{js,ts}'));
+files
+  .filter((filePath) => !/\.config\.js$/gi.test(path.basename(filePath)))
+  .forEach((filePath) => fs.removeSync(filePath));
+
+console.log(colors.blue('Restoring config files'));
+const tempFolder = path.join(process.cwd(), 'configTemp');
+const tempFolderFiles = glob.sync(path.join(tempFolder, '*'));
+tempFolderFiles.forEach((filePath) => {
+  fs.moveSync(filePath, path.join(process.cwd(), path.basename(filePath)));
 });
+fs.removeSync(tempFolder);
--- scripts/prepublish.js
+++ scripts/prepublish.js
@@ -9,10 +9,17 @@ console.log(colors.blue('Building Package Files'));
 require('./svg2Js.js');
 require('./svg2Font.js');
 
-glob(path.join(process.cwd(), 'js/*.js'), {}, (err, files) => {
-  console.log(colors.blue('Moving to correct directory and cleaning up.'));
-  files.forEach((filePath) =>
-    fs.moveSync(filePath, path.join(process.cwd(), path.basename(filePath)))
-  );
-  fs.removeSync(path.join(process.cwd(), 'js'));
+const configFiles = glob.sync(path.join(process.cwd(), '*.{js,ts}'));
+const tempFolder = path.join(process.cwd(), 'configTemp');
+
+fs.mkdirSync(tempFolder);
+configFiles.forEach((filePath) => {
+  fs.moveSync(filePath, path.join(tempFolder, path.basename(filePath)));
 });
+
+const files = glob.sync(path.join(process.cwd(), 'js/*.{js,ts}'));
+console.log(colors.blue('Moving to correct directory and cleaning up.'));
+files.forEach((filePath) =>
+  fs.moveSync(filePath, path.join(process.cwd(), path.basename(filePath)))
+);
+fs.removeSync(path.join(process.cwd(), 'js'));
